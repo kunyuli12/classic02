bc6b2c47d140df63741f2ffe0132ed036f3235d8
kunyuli <kunyuli16@gmail.com>
2021-09-05 00:00:57 +0800

    新增背景以及文字修改-1

--- classic02/ContentView.swift
+++ classic02/ContentView.swift
@@ -9,22 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hi")
-                .font(.largeTitle)
-                .padding(.top,40)
-            Text("Hello, world!")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .padding(.bottom,40)
-            Text("word")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-        }
+    VStack {
+      Text("kunyuli-30%完成度")
+        .padding(.bottom)
+      Text("Hello, world!")
+        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+        .padding(.bottom,40)
+      Text("word")
+        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+         }
         .frame(height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
         .frame(maxWidth:.infinity )
-        .background(Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)))
+    .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.8520846261, green: 0.8920375053, blue: 1, alpha: 1))]), startPoint: .top, endPoint: .bottom))
         .cornerRadius(25)
         .padding()
-        
+        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
     }
 }
 

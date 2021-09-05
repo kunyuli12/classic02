1ade03880549a8bf00a6846da3bb5dbf4b286747
kunyuli <kunyuli16@gmail.com>
2021-09-05 11:06:10 +0800

    文字的新增 增加image圖片 陰影處做調整(顏色、位置、陰影程度要多少)

--- classic02/ContentView.swift
+++ classic02/ContentView.swift
@@ -9,21 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-    VStack {
-      Text("kunyuli-30%完成度")
-        .padding(.bottom)
-      Text("Hello, world!")
-        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-        .padding(.bottom,40)
-      Text("word")
-        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-         }
-        .frame(height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+        VStack (alignment:.leading){
+            Text("kunyuli-30%完成度")
+                .padding(.bottom)
+            HStack {
+                Image(systemName: "gear")
+                Text("Account")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .bold()
+                    .padding(5)
+            }
+            HStack {
+                Image(systemName: "creditcard")
+                Text("Billing")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .bold()
+                    .padding(5)
+            }
+            HStack {
+                Image(systemName:"person.crop.circle" )
+                Text("Sign out")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .bold()
+                    .padding(5)
+            }
+        }
+        .frame(height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
         .frame(maxWidth:.infinity )
     .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.8520846261, green: 0.8920375053, blue: 1, alpha: 1))]), startPoint: .top, endPoint: .bottom))
         .cornerRadius(25)
         .padding()
-        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+    .shadow(color:Color.black.opacity(0.2),radius: 20,x:0,y:20)
     }
 }
 
